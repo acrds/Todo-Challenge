045e86b95207962a0c305d068f3eb8a51211a5bb
Ana Claudia Ribeiro dos Santos <anasantos.rds@outlook.com>
2024-11-24 12:39:57 -0300

    feat: create screens

--- App.js
+++ App.js
@@ -1,43 +1,15 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from '@react-navigation/elements';
+import HomeScreen from './src/screens/HomeScreen';
+import ConfigScreen from './src/screens/LoginScreen';
 
-
-function HomeScreen() {
-  const navigation = useNavigation();
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button onPress={() => navigation.navigate('Config')}>
-        Go to Config
-      </Button>
-    </View>
-  );
-}
-
-function ConfigScreen() {
-    const navigation = useNavigation();
-    
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Config Screen</Text>
-        <Button onPress={() => navigation.navigate('Home')}>
-          Go to Home
-        </Button>
-      </View>
-    );
-  }
-  
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Config">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Config" component={ConfigScreen} />
       </Stack.Navigator>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import  {useNavigation} from '@react-navigation/native';
 
-export default function ConfigScreen() {
+export default function HomeScreen() {
     const navigation = useNavigation();
-    
+  
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Config Screen</Text>
-        <Button title="Go to Home" onPress={() => navigation.navigate('Home')}/>
+        <Text>Home Screen</Text>
+        <Button title="Go to Config" onPress={() => navigation.navigate('Config')}/>
       </View>
     );
   }
